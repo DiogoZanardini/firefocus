510267416e381322d5d727e899616b94c26fb356
DiogoZanardini <diogozanardini@gmail.com>
2022-05-26 02:16:22 -0300

    Add files via upload

--- firefocus.py
+++ firefocus.py
@@ -1,56 +1,62 @@
-
-
 import streamlit as st
 from PIL import Image
 import pandas as pd
 import seaborn as sns
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 plt.style.use('seaborn')
 import statsmodels.api as sm
-
-
-x = pd.read_csv('focos_mensal.csv')
-
-model=sm.tsa.statespace.SARIMAX(x['total'],order=(0, 0, 2),
-                                  seasonal_order=(1, 1, 2, 12))
-results = model.fit()
-
-col1, col2 = st.columns(2)
-image = Image.open('img/logo.jpg')
-col1.image(image, width= 70,caption='FFP v1.0.0')
-image = Image.open('img/title.jpg')
-col2.image(image)
-st.markdown("Este é um Data App utilizado para exibir a solução de Inteligencia artificial para analise de possíveis focos de incêndio no Brasil. Este modelo preditivo está calibrado para aferir previsões aceitaveis até o mes de dezembro de 2022 ")
-st.markdown("Para visualizar as previsões, selecione o período:")
-
-
-meses_num = (53,54,55,56,57,58,59,60)
 
 meses = ('Maio','Junho',
         'Julho','Agosto','Setembro',
         'Outubro','Novembro','Dezembro')
 
-col1, col2 = st.columns(2)
-m_start = col1.selectbox('Mes inicial:', meses[:-1], index=0)
-m_start_index = meses.index(m_start)
-m_end   = col2.selectbox('Mes final:', meses[m_start_index+1:], index=0)
+x = pd.read_csv('focos_mensal.csv')
+x.index = pd.DatetimeIndex(x['dia'])
 
-m_start_num = meses_num[meses.index(m_start)]
-m_end_num   = meses_num[meses.index(m_end)]
+model=sm.tsa.statespace.SARIMAX(x['total'],order=(0, 0, 2),
+                                  seasonal_order=(1, 1, 2, 12))
+results = model.fit()
 
-btn1 = st.button('REALIZAR PREVISÃO')
-btn2 = st.button('LIMPAR')
+col1, col2= st.columns((1,4))
 
-if btn1:
+image = Image.open('img/logo.jpg')
+col1.image(image, width= 70,caption='FFP v1.0.0')
+image = Image.open('img/title.jpg')
+col2.image(image)
+col2.markdown("Este é um Data App utilizado para exibir a solução de Inteligencia artificial para analise de possíveis focos de incêndio no Brasil. Este modelo preditivo está calibrado para aferir previsões aceitaveis até o mes de dezembro de 2022 ")
+st.subheader("Para selecionar os período, deslise os botões abaixo:")
+
+m_start, m_end = st.select_slider('',options=meses, value=('Maio','Dezembro'))
+m_start_num    = meses.index(m_start)+52
+m_end_num      = meses.index(m_end)+52
+
+spacer, col1, spacer = st.columns((5,40,1))
+col1.write(f'{m_start} até {m_end}')
+
+col1, col2 = st.columns((4,1))
     
-    a = results.predict(start = m_start_num,
-                        end   = m_end_num)
+a = results.predict(start = m_start_num, end = m_end_num)
+fig, ax = plt.subplots(figsize=(15,6))
+plt.xlabel("\n Meses", size=28)
+plt.ylabel("Total de focos por dia\n ", size=28)
+ax.plot(a)
+ax.scatter(a.index, a)
 
-    fig = plt.figure(figsize=(14,6))
-    x['total'].plot()
-    a.plot()
-    st.pyplot(fig) 
+ax.xaxis.set_major_locator(mdates.MonthLocator())
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%b-%y'))
 
-if btn2:
-    st.markdown('')
-   
+plt.xticks(size=22)
+plt.yticks(size=22)
+
+col1.pyplot(fig)
+
+z= pd.DataFrame(a)
+z = z.rename(columns={"predicted_mean": "Focos"})
+z.index = z.index.strftime('%b-%y')
+z['Focos'] = round(z['Focos']).astype(int)
+col2.dataframe(z)
+
+
+
+
